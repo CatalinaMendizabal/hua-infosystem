5c9ff27daa0d3110bf73016c921ab3e617c85d57
agustinVon <aguvon2001@gmail.com>
2023-08-27 12:24:23 -0300

    feat: added record screen

--- src/screens/record/index.tsx
+++ src/screens/record/index.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import Structure from "../../components/structure";
 
 const RecordingScreen = ({}) => {
-    return <></>
+    const [minutes, setMinutes] = useState(0);
+    const [seconds, setSeconds] = useState(0);
+    const [startDate, setStartDate] = useState<Date | undefined>(undefined)
+
+    const getTime = () => {
+        if (startDate) {
+            const time = Date.now() - startDate.getTime();
+            setMinutes(Math.floor((time / 1000 / 60) % 60));
+            setSeconds(Math.floor((time / 1000) % 60));
+        }
+    };
+
+    // useEffect(() => {
+    //     const interval = setInterval(() => getTime(deadline), 1000);
+    //
+    //     return () => clearInterval(interval);
+    // }, []);
+
+    return <Structure>
+        <div></div>
+    </Structure>
 }
 
 export default RecordingScreen
